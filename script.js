07815398324476ddefb21fce9933656ac61b1896
rajkushwaha7584 <rj8517884737@gmail.com>
2025-04-21 01:57:33 +0530

    gallery and coding platform added

--- script.js
+++ script.js
@@ -16,6 +16,26 @@ AOS.init({
   
   });
   
-  
-  
+// gallery hover effect 
+  // document.querySelectorAll('.tilt-wrapper').forEach(wrapper => {
+  //   const card = wrapper.querySelector('.achievement-card');
+  //   wrapper.addEventListener('mousemove', e => {
+  //     const rect = wrapper.getBoundingClientRect();
+  //     const x = e.clientX - rect.left;
+  //     const y = e.clientY - rect.top;
+  //     const centerX = rect.width / 2;
+  //     const centerY = rect.height / 2;
+
+  //     const rotateX = -(y - centerY) / 15;
+  //     const rotateY = (x - centerX) / 15;
+
+  //     card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  //   });
+
+  //   wrapper.addEventListener('mouseleave', () => {
+  //     card.style.transform = 'rotateX(0deg) rotateY(0deg)';
+  //   });
+  // });
+
+
   
